cde56ca90a1b1f9b136f800fa0c53f610508ca6c
Soluciones Inteligentes <sinteligentesw@gmail.com>
2026-06-28 19:46:43 -0400

    add config build 2.0

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
@@ -22,7 +21,7 @@ export default defineConfig(({ mode }) => {
                     },
                 },
             }),
-            tailwindcss(),
+
         ],
         resolve: {
             alias: {
